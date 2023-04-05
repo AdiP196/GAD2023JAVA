f02e610988fab661c9046e85e000f03f305d7092
AdiP196 <adipopa229@gmail.com>
2023-04-05 19:12:50 +0300

    AA

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,13 +4,13 @@ public class FizzBuzz {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 if (i % 3 == 0) {
-                    System.out.println("Fizz");
+                    System.out.print("Fizz");
                 }
                 if (i % 5 == 0) {
-                    System.out.println("Buzz");
+                    System.out.print("Buzz");
                 }
             } else {
-                System.out.println(i);
+                System.out.print(i);
             }
             System.out.println(" ");
         }
