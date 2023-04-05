140fd8c330af0bd4e014c877f9cda0b7e75721f2
AdiP196 <adipopa229@gmail.com>
2023-04-05 19:06:18 +0300

    FizzBuzz

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static void copyArrayExample() {
+        char[] copyFrom = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        char[] copyTo = new char[7];
+        System.arraycopy(copyFrom,2,copyTo,0,7);
+        System.out.println(new String(copyTo));
+    }
+    static void compareString() {
+        String firstString = new String("FirstString");
+        String secondString = new String("SecondString");
+
+        if(firstString.equals(secondString)){
+            System.out.println("True");
+        }else System.out.println("False");
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            //copyArrayExample();
+            //byte a1 = 12;
+            //byte a2 = 56;
+            //byte a3 =(byte) (a1+ a2);
+            //System.out.println(a3);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            compareString();
         }
     }
-}
